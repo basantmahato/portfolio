fec7f42b91c508ad1823b4c3d155d219d815f4d9
Basant Kumar Mahato <basantkumar72580@gmail.com>
2025-10-02 17:04:40 +0530

    1.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// App.jsx (FIXED)
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
@@ -9,7 +9,7 @@ import Projects from './components/Projects';
 import ContactFooter from './components/ContactFooter';
 import ResumePage from './components/ResumePage';
 
-// Component to hold all the single-page content
+
 const HomePageContent = () => (
     <>
      <Navbar />
@@ -23,14 +23,14 @@ const HomePageContent = () => (
 function App() {
   return (
     <Router>
-        {/* Navbar is rendered outside Routes so it's always visible */}
+       
        
         
         <Routes>
-            {/* 1. Main Content Route: Renders everything on the homepage (path="/") */}
+           
             <Route path="/" element={<HomePageContent />} />
             
-            {/* 2. Resume Page Route: Renders ONLY the ResumePage at /resume */}
+          
             <Route path="/resume" element={<ResumePage />} />
         </Routes>
     </Router>
